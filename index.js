6da7eeb3dd7e67dbe4d710b2d9f1d27a93839357
OmarBenitezL <isaibenitezlopez@gmail.com>
2018-11-12 10:57:10 -0600

    Finished GraphQL course

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ const {
   GraphQLList,
   GraphQLNonNull,
   GraphQLID,
-  GraphQLString,
-  GraphQLInt,
+  GraphQLString, GraphQLInt,
   GraphQLBoolean,
 } = require('graphql');
 const { getVideoById, getVideos, createVideo } = require('./src/data');
+const {
+  globalIdField,
+  connectionDefinitions,
+  connectionFromPromisedArray,
+  connectionArgs,
+  mutationWithClientMutationId,
+} = require('graphql-relay');
+const { nodeInterface, nodeField } = require('./src/node');
 
 const PORT = process.env.PORT || 3000;
 const server = express();
@@ -22,10 +29,7 @@ const videoType = new GraphQLObjectType({
   name: 'Video',
   description: 'A video on Egghead.io',
   fields: {
-    id: {
-      type: GraphQLID,
-      description: 'The id of the video.',
-    },
+    id: globalIdField(),
     title: {
       type: GraphQLString,
       description: 'The title of the video.',
@@ -39,15 +43,35 @@ const videoType = new GraphQLObjectType({
       description: 'Whether or not the video has been released.',
     },
   },
+  interfaces: [nodeInterface],
+});
+exports.videoType = videoType;
+
+const { connectionType: VideoConnection } = connectionDefinitions({
+  nodeType: videoType,
+  connectionFields: () => ({
+    totalCount: {
+      type: GraphQLInt,
+      description: 'A count of the total number of objects in this connection.',
+      resolve: (conn) => {
+        return conn.edges.length;
+      },
+    },
+  }),
 });
 
 const queryType = new GraphQLObjectType({
   name: 'QueryType',
   description: 'The root query type.',
   fields: {
+    node: nodeField,
     videos: {
-      type: new GraphQLList(videoType),
-      resolve: getVideos,
+      type: VideoConnection,
+      args: connectionArgs,
+      resolve: (_, args) => connectionFromPromisedArray(
+        getVideos(),
+        args
+      ),
     },
     video: {
       type: videoType,
@@ -64,9 +88,9 @@ const queryType = new GraphQLObjectType({
   },
 });
 
-const videoInputType = new GraphQLInputObjectType({
-  name: 'VideoInput',
-  fields: {
+const videoMutation = mutationWithClientMutationId({
+  name: 'AddVideo',
+  inputFields: {
     title: {
       type: new GraphQLNonNull(GraphQLString),
       description: 'The title of the video.',
@@ -80,23 +104,23 @@ const videoInputType = new GraphQLInputObjectType({
       description: 'Whether or not the video is released.',
     },
   },
+  outputFields: {
+    video: {
+      type: videoType,
+    },
+  },
+  mutateAndGetPayload: (args) => new Promise((resolve, reject) => {
+    Promise.resolve(createVideo(args))
+      .then((video) => resolve({ video }))
+      .catch(reject);
+  }),
 });
 
 const mutationType = new GraphQLObjectType({
   name: 'Mutation',
   description: 'The root Mutation type.',
   fields: {
-    createVideo: {
-      type: videoType,
-      args: {
-        video: {
-          type: new GraphQLNonNull(videoInputType),
-        },
-      },
-      resolve: (_, args) => {
-        return createVideo(args.video);
-      },
-    },
+    createVideo: videoMutation,
   },
 });
 
